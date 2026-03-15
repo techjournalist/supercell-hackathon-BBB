9fc951675423feef221aa50db82bb1e600694120
techjournalist <44219393+techjournalist@users.noreply.github.com>
2026-03-14 21:01:58 -0400

    Fix Supabase install with CDN shim

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@supabase/supabase-js': path.resolve('./supabase-cdn-shim.js'),
+    },
+  },
   server: {
     host: true,
     port: 5173,
